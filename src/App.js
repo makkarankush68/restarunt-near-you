01524fded9f95d0e0d7aa681aa46208da9efff9f
Ankush Makkar <ankushmakkar108@gmail.com>
2024-03-15 23:51:36 +0530

    fix lazy load

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import ShimmerUi from "./components/ShimmerUi";
